d7dd92a1f96bd07dfbd783f91331ffdfae251ab9
hwalker928 <harry@repoleved.xyz>
2020-12-11 18:48:43 +0000

    ✅ Completed Task 42
    Used the socket library to check the port of the specified host.

---
+++ 42/scanner.py
@@ -0,0 +1,15 @@
+from socket import *
+
+target = input('Enter the host to scan: ')
+port = input("Enter port to scan: ")
+port = int(port)
+t_IP = gethostbyname(target)
+
+print (f'\nPlease wait while we scan {t_IP}.')
+s = socket(AF_INET, SOCK_STREAM)
+conn = s.connect_ex((t_IP, port))
+if int(conn) == 0:
+    print(f"\n{t_IP} has {port} OPEN.\n")
+else:
+    print(f"\n{t_IP} has {port} CLOSED.\n")
+s.close()
